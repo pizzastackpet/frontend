a3a76b0cbae9bc148c8ae8e54182a34f0fe7ac4d
wiNgeRf0rN1ceE <ostapenko18.yuri@gmail.com>
2022-11-15 01:13:45 +0200

    Install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,6 @@
 export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 className="font-bold text-3xl text-red-600">Hello Brothers</h1>
   );
 }
 
