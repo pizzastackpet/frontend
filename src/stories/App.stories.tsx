0649c7b742b161cc7c70a5a86c656220b7129e2b
wiNgeRf0rN1ceE <ostapenko18.yuri@gmail.com>
2022-11-19 02:13:10 +0200

    Install craco

--- src/stories/App.stories.tsx
+++ src/stories/App.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { App } from '../App';
+import { App } from '@app/App';
 
 export default {
   title: 'Example/App',
